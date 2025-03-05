18159dc05b2bac9caecf063600ba0575415b2bc5
Nick French <64120654+nickjfrench@users.noreply.github.com>
2025-03-05 10:27:44 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# set default git config
+cp ./.gitconfig ~
+
+# oh-my-zsh & plugins
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
+zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+
+# copy in my p10k theme settings
+cp ./.p10k ~
+
+# Install p10k theme.
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+# remove newly created default p10k theme.
+rm -f ~/.p10k.zsh
+
+# update theme
+sed -i '/^ZSH_THEME/c\ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
+
+# use p10k.zsh config. 
+sed -i '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' 
